4f7bacf5fb00e8ed68f7e6fadbd815435a6e77c8
mmpro <marekmikolajczyk87@gmail.com>
2019-04-02 12:53:16 +0200

    debug host

--- register_page/settings.py
+++ register_page/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config('SECRET_KEY')
 # DEBUG = config('DEBUG', default=False, cast=bool)
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['webregisterpage.herokuapp.com']
 
 
 # Application definition
