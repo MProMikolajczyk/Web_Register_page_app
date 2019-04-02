a732bc66bc5ea786e12d21cedf447b382311669a
mmpro <marekmikolajczyk87@gmail.com>
2019-04-02 12:51:04 +0200

    debug True

--- register_page/settings.py
+++ register_page/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = config('DEBUG', default=False, cast=bool)
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
